52c0de83c57af3e101388d49505fe56ea0b5de8f
juancjara <juanc.jara@pucp.pe>
2015-08-10 15:25:37 -0400

    fixed error with favicon and adding clicks field

--- src/clj/url_shortener/actions.clj
+++ src/clj/url_shortener/actions.clj
@@ -1,7 +1,7 @@
 (ns url-shortener.actions
   (:use url-shortener.views))
 
-(def db (atom {:a "http://www.google.com"}))
+(def db (atom {}))
 
 (defn create-valid-url [url]
   (if (.contains url "http")
@@ -9,7 +9,14 @@
     (str "http://" url)))
 
 (defn look-up [url]
-  (create-valid-url (get @db (keyword url))))
+  (println "called")
+  (let [key (keyword url)
+        elem (get @db key)
+        new-url (first elem)
+        clicks (inc (second elem))]
+     (swap! db (fn[a] (assoc a key [new-url clicks])))
+     (println @db)
+     (create-valid-url new-url)))
 
 (defn add-elem [elem]
   (swap! db conj elem))
@@ -20,6 +27,5 @@
 (defn short-url [url-to-short headers]
   (let [hash-url (create-short-url url-to-short)
         base-url (second (first headers))]
-    (add-elem (hash-map (keyword hash-url) url-to-short))
+    (add-elem (hash-map (keyword hash-url) [url-to-short 0]))
     (str base-url "/" hash-url)))
-
--- src/clj/url_shortener/handler.clj
+++ src/clj/url_shortener/handler.clj
@@ -6,16 +6,10 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
-(defn ggwp [& data]
-  (println (str data))
-  (println data)
-  "testerino")
-
 (defroutes app-routes
   (GET "/" [] (index-page))
-  ;;(POST "/test" {{url :url} :params headers :headers}  (ggwp url headers))
   (POST "/short" {{url :url} :params headers :headers} (short-url url headers))
-  (GET "/:foo" {{foo :foo} :params } (redirect (look-up foo)))
+  (GET "/s/:foo" {{foo :foo} :params } (redirect (look-up foo)))
   (route/not-found "Not Found"))
 
 (def app
